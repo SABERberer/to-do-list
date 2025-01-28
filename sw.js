0fe1c652b7009da5b5d6ae8f53075d8f24e60012
SABERberer <142846519+SABERberer@users.noreply.github.com>
2025-01-28 16:47:40 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'todo-pwa-v3';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  // 添加其他需要缓存的资源路径
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+    ))
+  );
+});
